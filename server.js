75bd19938e9bbf95d5826d1fc374fe5356a9ca8e
Sai Teja <48650056+Chanti-123@users.noreply.github.com>
2020-05-21 13:05:48 +0530

    env file

--- server.js
+++ server.js
@@ -3,11 +3,18 @@ const path=require('path')
 
 const app=exp()
 
+app.use(exp.json())
+
 app.use(exp.static(path.join(__dirname,'./dist/farmersfriend')));
 
 app.get('/*',(req,res)=>{
     res.sendFile(path.join(__dirname + '/dist/farmersfriend/index.html'));
 })
 
-app.listen(5700);
+//importing dotenv module
+const env=require('dotenv').config()
+
+
+
+app.listen(process.env.port || 5700);
 
